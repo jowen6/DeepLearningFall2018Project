1d9c85e1ddd4b415159aa7e15ab6f239ef1b54ba
Justin Owen <justinowen@Justins-MacBook-Pro.local>
2018-11-07 22:10:58 -0600

    Created single layer nn to show universal approx thm

---
+++ FunctionApproximationNetwork.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 21:13:06 2018
+
+@author: justinowen
+"""
+import numpy
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+# torch.manual_seed(1)    # reproducible
+
+x = torch.unsqueeze(torch.linspace(0, 6, 200), dim=1)  # x data (tensor), shape=(100, 1)
+y = numpy.sin(x) + 0.2*torch.rand(x.size())                 # noisy y data (tensor), shape=(100, 1)
+
+# torch can only train on Variable, so convert them to Variable
+# The code below is deprecated in Pytorch 0.4. Now, autograd directly supports tensors
+# x, y = Variable(x), Variable(y)
+
+# plt.scatter(x.data.numpy(), y.data.numpy())
+# plt.show()
+
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden1 = torch.nn.Linear(n_feature, n_hidden)   # hidden layer
+        self.hidden2 = torch.nn.Linear(n_hidden, n_hidden)   # hidden layer
+        self.output = torch.nn.Linear(n_hidden, n_output)   # output layer
+
+    def forward(self, x):
+        x = F.relu(self.hidden1(x))      # activation function for hidden layer
+        #x = F.relu(self.hidden2(x))      # activation function for hidden layer
+        x = self.output(x)             # linear output
+        return x
+
+net = Net(n_feature=1, n_hidden=50, n_output=1)     # define the network
+print(net)  # net architecture
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.08, momentum = 0.7) #Use stochastic gradient descent
+loss_func = torch.nn.MSELoss()  # this is for regression mean squared loss
+
+plt.ion()   # turn interactive plotting on
+
+#Cycle through data 200 times for SGD
+for t in range(800):
+    prediction = net(x)     # input x and predict based on x
+
+    loss = loss_func(prediction, y)     # must be (1. nn output, 2. target)
+
+    optimizer.zero_grad()   # clear gradients for next train
+    loss.backward()         # backpropagation, compute gradients
+    optimizer.step()        # apply gradients
+
+    if t % 5 == 0:
+        # plot and show evolution of Net curve
+        plt.cla()
+        plt.scatter(x.data.numpy(), y.data.numpy())
+        plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+        plt.text(3.1, 0.5, 'Loss=%.6f' % loss.data.numpy(), fontdict={'size': 20, 'color':  'red'})
+        plt.pause(0.1)
+
+plt.ioff()
+plt.show()
