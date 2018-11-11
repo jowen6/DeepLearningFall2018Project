947754c1cd446ccebebf1de068a1e80390813299
Peng Wei <peng@Pengs-MacBook-Pro.local>
2018-11-10 22:25:11 -0600

    creates div_free files in csv format

--- VectorFieldGenerator.py
+++ VectorFieldGenerator.py
@@ -4,15 +4,38 @@
 Created on Thu Nov  8 18:47:26 2018
 
 @author: justinowen
+
+Updated: Nov. 10, 2018 22:12:45 Peng Wei
+Creates the div_free vector fields and save to csv file,
+with coefficients uniformly distributed in [0,1].
+
+
 """
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(-1,1,50)
-y = np.linspace(-1,1,50)
+
+x = np.linspace(-1,1,64)
+y = np.linspace(-1,1,64)
 X, Y = np.meshgrid(x,y)
-Vx = np.cos(10*Y*Y*Y)#2*np.sin(5*Y)
+Vx = np.cos(5*Y*Y)#2*np.sin(5*Y)
 Vy = np.sin(5*X)
+
+
+import csv
+
+for i in range(1000):
+    file_name = "./data/div_free_" + str(i).zfill(4) + ".csv"
+    s = np.random.uniform(0,1,8)
+    Vx = s[0]*np.cos(np.pi*Y) + s[1]*Y + s[2]*np.sin(np.pi*Y) + s[3]*np.exp(Y)/np.e
+    Vy = s[4]*np.cos(np.pi*X) + s[5]*X + s[6]*np.sin(np.pi*X) + s[7]*np.exp(X)/np.e
+    with open(file_name,"w+") as my_csv:
+        csvWriter = csv.writer(my_csv,delimiter=',')
+        csvWriter.writerows(Vx)
+        csvWriter.writerows("\n")
+        csvWriter.writerows(Vy)
+
+
 
 """
 for i in range(2):
@@ -35,13 +58,13 @@ qk = plt.quiverkey(Q, 0.9, 0.9, 2, r'$2 \frac{m}{s}$', labelpos='E',
                    coordinates='figure')
 """
 
-plt.figure()
-plt.title("pivot='mid'; every third arrow; units='inches'")
-Q = plt.quiver(X[::3, ::3], Y[::3, ::3], Vx[::3, ::3], Vy[::3, ::3],
-               pivot='mid', units='inches')
-qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
-                   coordinates='figure')
-#plt.scatter(X[::3, ::3], Y[::3, ::3], color='r', s=5)
+#plt.figure()
+#plt.title("pivot='mid'; every third arrow; units='inches'")
+#Q = plt.quiver(X[::3, ::3], Y[::3, ::3], Vx[::3, ::3], Vy[::3, ::3],
+#               pivot='mid', units='inches')
+#qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
+#                   coordinates='figure')
+##plt.scatter(X[::3, ::3], Y[::3, ::3], color='r', s=5)
 
 
 """
@@ -54,4 +77,4 @@ qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
                    coordinates='figure')
 plt.scatter(X, Y, color='k', s=5)
 """
-plt.show()
+#plt.show()
