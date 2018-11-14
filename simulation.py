1eb61a28f5c87de816c3d77bdfe715b16cbb6176
Peng Wei <peng@main-10-231-129-47.tamulink.tamu.edu>
2018-11-14 16:05:53 -0600

    added softmax

---
+++ simulation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 12:12:15 2018
+
+@author: peng
+"""
+
+import VectorFieldClassifier as vfc
+
+NumSimulations = 100
+
+vfc.my_classifier(NumSimulations)
+
