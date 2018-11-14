1a8b5686aa932e4c559925678545a64cc1a0ca84
Justin Owen <justinowen@main-10-231-66-104.tamulink.tamu.edu>
2018-11-14 11:03:04 -0600

    The classifier now works 99%

--- VectorFieldClassifier.py
+++ VectorFieldClassifier.py
@@ -9,6 +9,7 @@ Created on Thu Nov  8 11:13:05 2018
 """
 Sections marked with "<3<3<3" are ones I'm happy with for now.
 """
+#Need to normalize tensor data to reasonable range [-1,1].
 
 # -*- coding: utf-8 -*-
 
@@ -24,10 +25,10 @@ import VFields as vf
 import torch.utils.data as utils
 
 #Number of each type of vector field (make divisible by 2)
-NumTrainVecFields = 400 #Will create twice this number of samples 
-NumTestVecFields = 26   #Will create twice this number of samples
+NumTrainVecFields = 500 #Will create twice this number of samples 
+NumTestVecFields = 200   #Will create twice this number of samples
 
-
+"""
 #Generating vector fields
 TrainData, TrainDataClassification = vf.GenerateFieldDataset(NumTrainVecFields)
 TestData, TestDataClassification = vf.GenerateFieldDataset(NumTestVecFields)
@@ -39,6 +40,14 @@ vf.SaveFieldDataset(TrainDataClassification,"TrainClassification_1.txt")
 
 vf.SaveFieldDataset(TestData,"TestDataset_1.txt")
 vf.SaveFieldDataset(TestDataClassification,"TestClassification_1.txt")
+"""
+#Load Data if available
+
+TrainData = vf.LoadFieldDataset("TrainDataset_1.txt")
+TrainDataClassification = vf.LoadFieldDataset("TrainClassification_1.txt")
+
+TestData = vf.LoadFieldDataset("TestDataset_1.txt")
+TestDataClassification = vf.LoadFieldDataset("TestClassification_1.txt")
 
 
 #Transform to torch tensors
@@ -65,6 +74,7 @@ FieldTestDataloader = utils.DataLoader(FieldTestDataset, batch_size=4, shuffle=F
 classes = ('DivFree', 'NotDivFree')
 
 
+
 ########################################################################
 # 2. Define a Convolution Neural Network <3<3<3
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -80,10 +90,10 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(2, 6, 5) #2 inputs, 6 outputs, 5x5 convolution window
+        self.conv1 = nn.Conv2d(2, 6, 10) #2 inputs, 6 outputs, 5x5 convolution window
         self.pool = nn.MaxPool2d(2, 2)  #2x2 non-overlapping window that takes max in window
-        self.conv2 = nn.Conv2d(6, 16, 5)    #6 inputs, 16 outputs, 5x5 convolution window
-        self.fc1 = nn.Linear(16 * 13 * 13, 120) #16*5*5 inputs, 120 outputs
+        self.conv2 = nn.Conv2d(6, 16, 10)    #6 inputs, 16 outputs, 5x5 convolution window
+        self.fc1 = nn.Linear(16 * 9 * 9, 120) #16*5*5 inputs, 120 outputs
         self.fc2 = nn.Linear(120, 84)   #120 inputs, 84 outputs
         self.fc3 = nn.Linear(84, 2)    #84 inputs, 10 outputs
 
@@ -91,7 +101,7 @@ class Net(nn.Module):
         x = self.pool(F.relu(self.conv1(x)))    #Convolution->ReLU->Pooling
         x = self.pool(F.relu(self.conv2(x)))    #Convolution->ReLU->Pooling
         #print(x.size())
-        x = x.view(-1, 16 * 13 * 13)  #Convert all 2D arrays to 1D arrays
+        x = x.view(-1, 16 * 9 * 9)  #Convert all 2D arrays to 1D arrays
         x = F.relu(self.fc1(x))     #New layer
         x = F.relu(self.fc2(x))     #New layer
         x = self.fc3(x)             #Output layer
@@ -109,7 +119,7 @@ net = Net()
 import torch.optim as optim
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.1)
 
 ########################################################################
 # 4. Train the network  <3<3<3
@@ -118,8 +128,8 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 # This is when things start to get interesting.
 # We simply have to loop over our data iterator, and feed the inputs to the
 # network and optimize.
-
-for epoch in range(2):  # loop over the dataset multiple times
+print('Training...')
+for epoch in range(6):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(FieldTrainDataloader, 0):
@@ -149,18 +159,17 @@ print('Finished Training')
 # 4. Test the network   <3<3<3
 # ^^^^^^^^^^^^^^^^^^^^
 # Let's look at how the network performs on the test dataset.
-
+print('Testing...')
 correct = 0
 total = 0
 with torch.no_grad():
     for data in FieldTestDataloader:
-        inouts, labels = data
-        print(labels)
-        outputs = net(inputs)
-        _, predicted = torch.max(outputs.data, 1)
+        Testinputs, labels = data
+        Testoutputs = net(Testinputs)
+        print(outputs)
+        _, predicted = torch.max(Testoutputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
-
-print('Accuracy of the network on the 52 test Fields: %d %%' % (
+print('Accuracy of the network on the test Fields: %d %%' % (
     100 * correct / total))
 ########################################################################
